563d551b1a6cf15082c5e534b5565a3900446877
z1haze <stephen.hendricks87@gmail.com>
2022-07-20 15:34:05 -0700

    add deleted_at field so soft delete channels and roles

---
+++ db/migrations/20220720204741_add_deleted_at_timestamp_to_discord_roles_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns {Knex.SchemaBuilder}
+ */
+exports.up = function up(knex) {
+  return knex.schema.table('discord_roles', (table) => {
+    table.timestamp('deleted_at');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns {Knex.SchemaBuilder}
+ */
+exports.down = function down(knex) {
+  return knex.schema.table('discord_roles', (table) => {
+    table.dropColumn('deleted_at');
+  });
+};
